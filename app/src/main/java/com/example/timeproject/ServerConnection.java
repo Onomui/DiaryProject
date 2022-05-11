b645693eac5a08720067a18b2fdf6bd719327149
ClayMix <claymix007@gmail.com>
2022-05-12 00:28:57 +0500

    Preparing methods for HTTP requests

--- app/src/main/java/com/example/timeproject/ServerConnection.java
+++ app/src/main/java/com/example/timeproject/ServerConnection.java
@@ -1,16 +1,29 @@
 package com.example.timeproject;
 
+import java.util.ArrayList;
+
 public class ServerConnection {
     public String URL;
     public final String SERVER_ALL_USERS = "/api/users";
     public final String SERVER_REGISTER = "/api/register";
     public final String SERVER_LOGIN = "/api/login";
 
-    public String getServerOnlyUser(String userEmail){
-        return SERVER_ALL_USERS + userEmail;
+    public boolean login(String email, String password){
+        return false;
     }
 
-    public String deleteUser(String userEmail){
-        return SERVER_ALL_USERS + userEmail;
+    public boolean register(String email, String name, String password){
+        return false;
     }
+
+    public ArrayList<String> getUser(String email){
+        ArrayList<String> userInfo = new ArrayList<>();
+        return userInfo;
+    }
+
+    public ArrayList<ArrayList<String>> getAllUsers(){
+        ArrayList<ArrayList<String>> all_users = new ArrayList<>();
+        return all_users;
+    }
+
 }
